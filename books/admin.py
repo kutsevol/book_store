30f8eb0a2ecef5abe51b602e3ea8bdacb41e3647
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-09-15 13:11:48 +0300

    Add 3 models: Author, Book and extra model for ManyToMany relations

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Authors, Books, AuthorsToBooks
+
+
+class AuthorsInline(admin.TabularInline):
+    model = AuthorsToBooks
+    extra = 0
+
+
+@admin.register(Books)
+class AdminBooks(admin.ModelAdmin):
+    inlines = [AuthorsInline]
+
+
+@admin.register(Authors)
+class AdminAuthors(admin.ModelAdmin):
+    pass
--- books/models.py
+++ books/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Authors(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=200)
+    authors = models.ManyToManyField(Authors, blank=False,
+                                     through='AuthorsToBooks')
+    isbn = models.CharField(max_length=20)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+
+    @property
+    def all_authors(self):
+        return ', '.join(' '.join((author.last_name, author.first_name))
+                         for author in self.authors.all())
+
+    def __str__(self):
+        return f"{self.all_authors} - {self.title}"
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+
+class AuthorsToBooks(models.Model):
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    author = models.ForeignKey(Authors, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ''
+
+    class Meta:
+        verbose_name = 'Author to book'
+        verbose_name_plural = 'Authors to book'
