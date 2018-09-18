9f426a81967a9adcb92579a825aeba54c051e9fc
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-09-18 23:34:12 +0300

    Rerange urls. Add delete endpoint

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import IndexView, EditBook, AddBook, ViewLogs
+from .views import IndexView, EditBook, AddBook, DeleteBook, ViewLogs
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='books'),
-    path('edit/<int:pk>/', EditBook.as_view(), name='book_edit'),
     path('add/', AddBook.as_view(), name='book_add'),
     path('logs/', ViewLogs.as_view(), name='logs'),
+    path('delete/<int:pk>/', DeleteBook.as_view(), name='book_delete'),
+    path('edit/<int:pk>/', EditBook.as_view(), name='book_edit'),
 ]
